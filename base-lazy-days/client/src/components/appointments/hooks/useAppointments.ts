daaaf2e63107dcf340b715fc1bcceffe334e38ac
Yeolha Sung <tjddufgk@gmail.com>
2022-09-29 20:12:10 +0900

    React-query Re-Fetching data

--- base-lazy-days/client/src/components/appointments/hooks/useAppointments.ts
+++ base-lazy-days/client/src/components/appointments/hooks/useAppointments.ts
@@ -1,7 +1,13 @@
 // @ts-nocheck
 import { useQuery, useQueryClient } from '@tanstack/react-query';
 import dayjs from 'dayjs';
-import { Dispatch, SetStateAction, useEffect, useState } from 'react';
+import {
+  Dispatch,
+  SetStateAction,
+  useCallback,
+  useEffect,
+  useState,
+} from 'react';
 
 import { axiosInstance } from '../../../axiosInstance';
 import { queryKeys } from '../../../react-query/constants';
@@ -9,6 +15,11 @@ import { useUser } from '../../user/hooks/useUser';
 import { AppointmentDateMap } from '../types';
 import { getAvailableAppointments } from '../utils';
 import { getMonthYearDetails, getNewMonthYear, MonthYear } from './monthYear';
+
+const commonOptions = {
+  staleTime: 0,
+  cacheTime: 300000,
+};
 
 // for useQuery call
 async function getAppointments(
@@ -60,9 +71,23 @@ export function useAppointments(): UseAppointments {
   //   appointments that the logged-in user has reserved (in white)
   const { user } = useUser();
 
+  const selectFn = useCallback((data) => getAvailableAppointments(data, user), [
+    user,
+  ]);
+
   /** ****************** END 2: filter appointments  ******************** */
   /** ****************** START 3: useQuery  ***************************** */
   // useQuery call for appointments for the current monthYear
+  const queryClient = useQueryClient();
+
+  useEffect(() => {
+    const nextMonthYear = getNewMonthYear(monthYear, 1);
+    queryClient.prefetchQuery(
+      [queryKeys.appointments, nextMonthYear.year, nextMonthYear.month],
+      () => getAppointments(nextMonthYear.year, nextMonthYear.month),
+      { ...commonOptions },
+    );
+  }, [queryClient, monthYear]);
 
   // TODO: update with useQuery!
   // Notes:
@@ -76,17 +101,15 @@ export function useAppointments(): UseAppointments {
   const { data: appointments = fallback } = useQuery(
     [queryKeys.appointments, monthYear.year, monthYear.month],
     () => getAppointments(monthYear.year, monthYear.month),
+    {
+      select: showAll ? undefined : selectFn,
+      ...commonOptions,
+      refetchOnMount: true,
+      refetchOnWindowFocus: true,
+      refetchOnReconnect: true,
+      refetchInterval: 60000,
+    },
   );
-
-  const queryClient = useQueryClient();
-
-  useEffect(() => {
-    const nextMonthYear = getNewMonthYear(monthYear, 1);
-    queryClient.prefetchQuery(
-      [queryKeys.appointments, nextMonthYear.year, nextMonthYear.month],
-      () => getAppointments(nextMonthYear.year, nextMonthYear.month),
-    );
-  }, [queryClient, monthYear]);
 
   /** ****************** END 3: useQuery  ******************************* */
   return { appointments, monthYear, updateMonthYear, showAll, setShowAll };
--- base-lazy-days/client/src/components/staff/hooks/useStaff.ts
+++ base-lazy-days/client/src/components/staff/hooks/useStaff.ts
@@ -1,5 +1,5 @@
 import { useQuery } from '@tanstack/react-query';
-import { Dispatch, SetStateAction, useState } from 'react';
+import { Dispatch, SetStateAction, useCallback, useState } from 'react';
 
 import type { Staff } from '../../../../../shared/types';
 import { axiosInstance } from '../../../axiosInstance';
@@ -21,7 +21,14 @@ export function useStaff(): UseStaff {
   const fallback = [];
   const [filter, setFilter] = useState('all');
 
-  const { data: staff = fallback } = useQuery([queryKeys.staff], getStaff);
+  const selectFn = useCallback(
+    (unFilteredStaff) => filterByTreatment(unFilteredStaff, filter),
+    [filter],
+  );
+
+  const { data: staff = fallback } = useQuery([queryKeys.staff], getStaff, {
+    select: filter === 'all' ? undefined : selectFn,
+  });
 
   return { staff, filter, setFilter };
 }
